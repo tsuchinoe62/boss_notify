40ebf8ea656c5e920222cbff9c5a7370e1296fbf
tsuchinoe62 <118424384+tsuchinoe62@users.noreply.github.com>
2024-06-05 13:26:20 +0000

    use table2ascii

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 import os
-import discord
-from discord import Intents, Client, Interaction, Embed, Message, app_commands
-from discord.app_commands import CommandTree, Choice
+import datetime
+from discord import Intents, Client, Embed
 from discord.ext import tasks
 from dotenv import load_dotenv
-import datetime
 import pandas as pd
-import numpy as np
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pytz
-from tabulate import tabulate
-from google.cloud import storage
+from table2ascii import table2ascii
 
 load_dotenv(verbose=True)
 CHANNEL_IDS = list(map(int, os.environ.get("CHANNEL_IDS").split(",")))
 TOKEN = os.environ.get("TOKEN")
 
-
-
 def get_upcoming_bosses() -> pd.DataFrame:
     auth = "./credentials.json"
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = auth
 
-    storage_client = storage.Client()
-    buckets = list(storage_client.list_buckets())
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(auth, scope)
-    Client = gspread.authorize(credentials)
+    oauth2_client = gspread.authorize(credentials)
 
-    sheet = Client.open_by_key("1osMHLFRGKJBNyCp8E3YcRNJFqCZycksXJ9XPyXNWW8Q")
+    sheet = oauth2_client.open_by_key("1osMHLFRGKJBNyCp8E3YcRNJFqCZycksXJ9XPyXNWW8Q")
     raw_data = sheet.worksheet("シート1")
     df = pd.DataFrame(raw_data.get_all_values())
 
-    return df.iloc[2:, 0:4]
+    return df.iloc[2:, 1:4]
 
 def display_upcoming_bosses(df: pd.DataFrame) -> Embed:
-    upcoming_bosses = tabulate(df, headers = ["場所", "名前", "時刻"], showindex=None)
-
-    upcoming_bosses += f"\n※最終更新: {datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M')}"
+    upcoming_bosses = table2ascii(
+        header=["場所", "名前", "時刻"],
+        body = df.values.tolist(),
+        footer=[
+            "最終更新:",
+            datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M'),
+            "",
+        ]
+    )
 
     embed = Embed(title="フィールドボス出現予定一覧", description=upcoming_bosses)
 
@@ -70,10 +68,10 @@ class MyClient(Client):
                 await self.messages[channel_id].edit(embed=embed)
             except Exception as e:
                 print(e)
-    
+
     async def on_ready(self) -> None:
         print(f"Logged in as {self.user}")
-        
+
         await self.init_bosses_list()
         self.update_bosses_list.start()
 
